4dbe04076d6c2ef12abde8ca6da27eaec400a2b7
Alex Noir <Varsash@gmail.com>
2021-10-04 03:21:59 +0300

    Improve player script by actually using the delta Add arena, bullets, bullet hell spawner scripts Implement the initial bullet hell spawning system thanks to https://worldeater-dev.itch.io/bittersweet-birthday/devlog/210789/howto-drawing-a-metric-ton-of-bullets-in-godot - this will change in the future Add bullet hitting player logic and bullets getting deleted after hitting player

---
+++ Arena.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+onready var bullet_spawner_area = $BulletSpawnerArea
+onready var bullet_spawn_pos = $ArenaCenter/BulletSpawn
+onready var bullet_spawn_dir = $ArenaCenter/BulletSpawn/Dir
+onready var timer = $Timer
+
+export var bullet_spawn_speed := 0.4 setget _timer_set
+export var bullet_count := 5
+export var bullet_speed := 100
+export var bullet_spawn_spread := 90
+
+export var pattern_speed := 0.5
+
+var boundary_rect: Rect2
+var hits = 0
+
+func _ready() -> void:
+	# Here we register the boundary
+	boundary_rect = Rect2(
+		$BulletBounds.rect_position,
+		$BulletBounds.rect_size
+	)
+	bullet_spawner_area.set_bounding_box(boundary_rect)
+	$BulletAnimation.play("main", -1, pattern_speed)
+
+func _timer_set(bullet_spawn_speed):
+	if timer:
+		timer.wait_time = bullet_spawn_speed / pattern_speed
+
+# Spawn a bullet wave with a slightly different rotation
+func _on_Timer_timeout() -> void:
+	var rotation_difference = bullet_spawn_spread / (bullet_count - 1)
+	var spawn_vector = bullet_spawn_pos.global_position.direction_to(bullet_spawn_dir.global_position)
+	for i in range(0, bullet_count):
+		var movement = spawn_vector.rotated(
+			deg2rad((rotation_difference * i) - 45)
+		)
+		bullet_spawner_area.spawn_bullet(bullet_spawn_pos.global_position, movement, bullet_speed * pattern_speed)
+
+
+func _on_Player_hit(bullet_id):
+	bullet_spawner_area.delete_bullet(bullet_id)
+	hits += 1
+	$HitCount.text = "Hits: " + str(hits)
+	$Player.modulate.a = 0.3
+	yield(get_tree().create_timer(.1), "timeout")
+	$Player.modulate.a = 0.7
+	yield(get_tree().create_timer(.1), "timeout")
+	$Player.modulate.a = 0.3
+	yield(get_tree().create_timer(.1), "timeout")
+	$Player.modulate.a = 1.0
--- Player.gd
+++ Player.gd
@@ -1,18 +1,22 @@
 extends KinematicBody2D
 
+signal hit(bullet_id)
 var direction = Vector2()
 var velocity = Vector2()
 export (int) var speed = 100
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	direction = Vector2(
 		int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
 		int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	)
 	direction = direction.normalized()
-	var _speed = speed
+	var _speed = speed * 60
 	if Input.is_action_pressed("ui_cancel"):
 		_speed *= 0.5
-	velocity = direction * _speed
-	move_and_slide(velocity)
-	position = position.round() # Faking integer precision
+	velocity = direction * _speed * delta
+	var _c = move_and_slide(velocity)
+
+
+func _on_Bullet_Hitbox_area_shape_entered(area_id, area, area_shape, local_shape):
+	emit_signal("hit", area_shape)
